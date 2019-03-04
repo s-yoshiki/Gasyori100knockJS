f095ecebc23d3036ac191d080873e65ae6d01947
Shinagawa Yoshiki <s.yoshiki1123@gmail.com>
2019-03-05 00:15:51 +0900

    ans32のdescription追加

--- src/components/questions/component.js
+++ src/components/questions/component.js
@@ -65,7 +65,7 @@ const componentMap = {
   "ans29": new Ans29(),
   "ans30": new Ans30(),
   "ans31": new Ans31(),
-  // "ans32": new Ans32(),
+  "ans32": new Ans32(),
   // "ans33": new Ans33(),
   // "ans34": new Ans34(),
   // "ans35": new Ans35(),
